44d127a851f934734c34f1436362a5ee84d13eb6
christoph <christoph@lafkon.net>
2021-01-07 22:05:48 +0100

    support flags (--out,--nolabel,--encode,--shorturl)

--- utils/qurl.sh
+++ utils/qurl.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-  OUT="qr"
-  TMP="XX"
   SHORTURLBASE="http://lfkn.de"
-  QRSEED=`echo $* | sed 's/ /\n/g' | grep -v '^--'`
+  TMP="XX";QRSEED=`echo $* | sed 's/ /\n/g' | grep -v '^--'`
+# --------------------------------------------------------------------------- #
+  SHORTURL=`echo $*            | #
+            sed 's/ /\n/g'     | #
+            grep '^--shorturl' | #
+            cut -d '=' -f 2`
+   NOLABEL=`echo $*            | #
+            sed 's/ /\n/g'     | #
+            grep '^--nolabel'`   #
+    ENCODE=`echo $*            | #
+            sed 's/ /\n/g'     | #
+            grep '^--encode'   | #
+            cut -d '=' -f 2`
+       OUT=`echo $*            | #
+            sed 's/ /\n/g'     | #
+            grep '^--out'      | #
+            cut -d '=' -f 2`;       if [ "$OUT" == "" ];then OUT="qr";fi
 # --------------------------------------------------------------------------- #
   function w() { echo "$*" >> $W; }
 # --------------------------------------------------------------------------- #
@@ -22,12 +36,21 @@
                          echo "${ID1}${ID2}"
   }
 # --------------------------------------------------------------------------- #
-  QRTXT=`shortRef $QRSEED      | #
-         tr [:lower:] [:upper:]` #
-  QRURL="$SHORTURLBASE/$QRTXT"
+  if [ "$ENCODE" == "" ]
+  then # -------------------------------------- #
+       if [ "$SHORTURL" == "" ]
+       then  QRURL=`shortRef $QRSEED      | #
+                    tr [:lower:] [:upper:]` #
+       else  QRURL=`echo $SHORTURL        | #
+                    tr [:lower:] [:upper:]` #
+       fi  # -------------------------------- #
+             QRCONTENT="$SHORTURLBASE/$QRURL"
+       # -------------------------------------- #
+  else       QRCONTENT="$ENCODE"
+  fi;        echo "$QRCONTENT"
 # --------------------------------------------------------------------------- #
-  echo "$QRURL" | qrencode -iv 1 -t EPS -o ${TMP}.eps
-  inkscape --export-plain-svg=${TMP}.1.svg  ${TMP}.eps
+  echo "$QRCONTENT" | qrencode -iv 1 -m 0 -t EPS -o ${TMP}.eps
+  inkscape --export-plain-svg=${TMP}.1.svg ${TMP}.eps
 # --------------------------------------------------------------------------- #
   W="${TMP}.2.svg";if [ -f $W ];then rm $W;fi
 # --------------------------------------------------------------------------- #
@@ -50,24 +73,29 @@
   w '<path id="frame" d="m 61,64 75,0 L 136,139 61,139 z"'
   w '       style="fill:#ffffff;stroke:#000000;stroke-width:1px;"/>'
   w '</g>'
-  w '<g id="l" inkscape:label="LABEL" inkscape:groupmode="layer">'
-  w '<text id="label" x="62" y="155"'
-  w '      xml:space="preserve"'
-  w '      sodipodi:linespacing="125%"'
-  w '      style="font-family:Roboto Mono;'
-  w '             -inkscape-font-specification:Roboto Mono Medium;'
-  w '             font-size:14px;font-style:normal;font-variant:normal;'
-  w '             font-weight:500;font-stretch:normal;text-indent:0;'
-  w '             text-align:start;text-decoration:none;'
-  w '             line-height:125%;letter-spacing:8px;word-spacing:0px;'
-  w '             text-transform:none;direction:ltr;'
-  w '             block-progression:tb;writing-mode:lr-tb;'
-  w '             text-anchor:start;baseline-shift:baseline;'
-  w '             fill:#000000;stroke:none;"><tspan id="t"'
-  w "      sodipodi:role=\"line\">$QRTXT</tspan></text>"
-  w '</g>'
+  if [ "$ENCODE" != "" ] || [ "$NOLABEL" == "" ]
+  then  w '<g id="l" inkscape:label="LABEL" inkscape:groupmode="layer">'
+        w '<text id="label" x="62" y="155"'
+        w '      xml:space="preserve"'
+        w '      sodipodi:linespacing="125%"'
+        w '      style="font-family:Roboto Mono;'
+        w '             -inkscape-font-specification:Roboto Mono Medium;'
+        w '             font-size:14px;font-style:normal;font-variant:normal;'
+        w '             font-weight:500;font-stretch:normal;text-indent:0;'
+        w '             text-align:start;text-decoration:none;'
+        w '             line-height:125%;letter-spacing:8px;word-spacing:0px;'
+        w '             text-transform:none;direction:ltr;'
+        w '             block-progression:tb;writing-mode:lr-tb;'
+        w '             text-anchor:start;baseline-shift:baseline;'
+        w '             fill:#000000;stroke:none;"><tspan id="t"'
+        w "      sodipodi:role=\"line\">$QRURL</tspan></text>"
+        w '</g>'
+  fi
 # --------------------------------------------------------------------------- #
-  SCALE="scale(0.1,-0.1)";MOVE="translate(55,145)"
+  QRW=`identify -format "%w" ${TMP}.eps`
+  S1="0.1055";S2=`python -c "print 63.00 / $QRW"`
+  S=`python -c "print round($S1 * $S2,4)"`
+  SCALE="scale($S,-$S)";MOVE="translate(65,135)"
   TRANSFORMQR="transform=\"$MOVE$SCALE\""
   w "<g $TRANSFORMQR>"
   sed ':a;N;$!ba;s/\n/ /g' ${TMP}.1.svg | #
