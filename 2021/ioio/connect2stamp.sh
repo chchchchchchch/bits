1da84c013bf8eac3a0afce1beefae29841b76f86
christoph <christoph@lafkon.net>
2021-06-08 23:01:53 +0200

    consider $MAYROTATE/$MAYFLIP

--- 2021/ioio/connect2stamp.sh
+++ 2021/ioio/connect2stamp.sh
@@ -6,6 +6,10 @@
 
   SRC=$1
   CONNECTORS=$2
+# --
+  MAYROTATE="R+";MAYFLIP="\-M+";
+# ----------------------------------------------------------------------- #
+  TMP="tmptmp";TIME=`date +%s`
 # ----------------------------------------------------------------------- #
   # ----------------------------------------------------------------- #
     C=`echo $CONNECTORS | cut -d ":" -f 1`;
@@ -25,35 +29,46 @@
     if [ "$T" = FLOP180 ];then C1=`c 1`;C2=`c 4`;C3=`c 3`;C4=`c 2`;fi
     if [ "$T" = FLOP270 ];then C1=`c 4`;C2=`c 3`;C3=`c 2`;C4=`c 1`;fi
   # ----------------------------------------------------------------- #
-    CGREP=`echo "connect=\"${C1}......\"|\
-                 connect=\"..${C2}....\"|\
-                 connect=\"....${C3}..\"|\
-                 connect=\"......${C4}\"" | #
+#   CGREP=`echo "connect=\"${C1}......\"|\
+#                connect=\"..${C2}....\"|\
+#                connect=\"....${C3}..\"|\
+#                connect=\"......${C4}\"" | #
+#          sed ':a;N;$!ba;s/\n//g'  | #
+#          sed 's/ //g'`
+    CGREP=`echo "connect=\"[A-Z0-9\.]*${C1}[A-Z0-9\.]*\"|\
+                 connect=\"[A-Z0-9\.]*${C2}[A-Z0-9\.]*\"|\
+                 connect=\"[A-Z0-9\.]*${C3}[A-Z0-9\.]*\"|\
+                 connect=\"[A-Z0-9\.]*${C4}[A-Z0-9\.]*\"" | #
            sed ':a;N;$!ba;s/\n//g'  | #
            sed 's/ //g'`
 # ----------------------------------------------------------------------- #
-  TMP="tmptmp";TIME=`date +%s`
+  SRCSRC=`sed ':a;N;$!ba;s/\n/ /g' ${SRC}` # LOAD INTO VARIABLE
 # ----------------------------------------------------------------------- #
 # GENERATE CODE FOR FOR-LOOP TO EVALUATE COMBINATIONS
 # ----------------------------------------------------------------------- #
   LOOPSTART="";VARIABLES="";LOOPCLOSE="";CNT=0
-  for BASETYPE in `sed ':a;N;$!ba;s/\n/ /g' ${SRC}   | #
+  for BASETYPE in `echo ${SRCSRC}                    | #
                    sed 's/<g/\n&/g'                  | # GROUPS ON NEWLINE
-                   sed '/^<g/s/>/&\n/g'              | # FIRST ON '>' ON NEWLINE
+                   sed '/^<g/s/>/&\n/g'              | # FIRST '>' ON NEWLINE
                    grep ':groupmode="layer"'         | #
                    egrep "$CGREP"                    | #
                    sed '/^<g/s/scape:label/\nlabel/' | #
                    grep ^label                       | #
                    grep -v 'label="XX_'              | #
                    cut -d "\"" -f 2                  | #
+                   cut -d "-" -f 1                   | #
                    sort -u`
-   do ALLOFTYPE=`sed ':a;N;$!ba;s/\n/ /g' ${SRC}              | #
-                 sed 's/scape:label/\nlabel/g'                | #
-                 grep ^label                                  | #
-                 grep -v 'label="XX_'                         | #
-                 cut -d "\"" -f 2                             | #
+   do ALLOFTYPE=`echo ${SRCSRC}                      | #
+                 sed 's/<g/\n&/g'                     | # GROUPS ON NEWLINE
+                 sed '/^<g/s/>/&\n/g'                  | # FIRST '>' ON NEWLINE
+                 grep ':groupmode="layer"'              | #
+                 egrep "$CGREP"                          | #
+                 sed 's/scape:label/\nlabel/g'            | #
+                 grep ^label                               | #
+                 grep -v 'label="XX_'                       | #
+                 cut -d "\"" -f 2                            | #
                  egrep "${BASETYPE}[-_]+[0-9]+|^${BASETYPE}$" | #
-                 sort -u`                                       #
+                 sort -u`                                        #
       LOOPSTART=${LOOPSTART}"for V$CNT in $ALLOFTYPE;do "
       VARIABLES=${VARIABLES}'$'V${CNT}" "
       LOOPCLOSE=${LOOPCLOSE}"done; "
@@ -66,112 +81,93 @@
   eval ${LOOPSTART}" echo $VARIABLES >> $KOMBILIST ;"${LOOPCLOSE}
 # ----------------------------------------------------------------------- #
   for KOMBI in `cat $KOMBILIST | sed 's/ /::SP::/g'`
-   do KOMBI=`echo $KOMBI | sed 's/::SP::/ /g'`
-#     R=`basename $SRC | cut -d "_" -f 2 | #
-#        grep "R+" | sed 's/\(.*\)\(R+\)\(.*\)/\2/g'`
-#     M=`basename $SRC | cut -d "_" -f 2 | #
-#        grep -- "-M[-+]*" | sed 's/\(.*\)\(M[-+]*\)\(.*\)/\2/g'`
-#   if [ "$M" == "M"  ];then M="-M-";fi
-#   if [ "$M" == "M-" ];then M="-M-";fi
-#   if [ "$M" == "M+" ];then M="+M-";fi
-#   if [ "$R" == "R+" ];then R="+R-";else R="";fi
-#   IOS=`basename $SRC | cut -d "_" -f 3- | cut -d "." -f 1`
-#   OUTPUTBASE=`basename $SRC            | #
-#               cut -d "_" -f 2          | #
-#               sed 's/-R+//g'           | #
-#               tr -t [:lower:] [:upper:]` #
-#   NID=`echo ${OUTPUTBASE}        | #
-#        cut -d "-" -f 1           | #
-#        tr -t [:lower:] [:upper:] | #
-#        md5sum | cut -c 1-4       | #
-#        tr -t [:lower:] [:upper:]`  #
-#   FID=`basename $SRC             | #
-#        tr -t [:lower:] [:upper:] | #
-#        md5sum | cut -c 1-4       | #
-#        tr -t [:lower:] [:upper:]`  #
-#   DIF=`echo ${KOMBI}${IOS}.svg   | #
-#        md5sum | cut -c 1-9       | #
-#        tr -t [:lower:] [:upper:] | #
-#        rev`                        #
-#   OUTNAME=$NID$FID`echo $R$M$DIF | rev          | #
-#                    sed 's/-M[-]*R+/-MR+/'       | #
-#                    rev | cut -c 1-9 | rev`_${IOS} #
-# ----------------------------------------------------------------------- #
-  LAYERGREP=`echo $KOMBI | sed 's/ /|/g'`
-# LAYERS=`sed ':a;N;$!ba;s/\n/ /g' ${SRC} | #
-#         sed 's/<g/\n&/g'                | # GROUPS ON NEWLINE
-#         sed '/^<g/s/>/&\n/g'            | # FIRST ON '>' ON NEWLINE
-#         grep ':groupmode="layer"'       | #
-#         egrep "$LAYERGREP"`               #   
-# ----------------------------------------------------------------------- #
-# if [ "_$IOS" == "_XX_XX_XX_XX_" ]
-# then  TOP=`echo $LAYERS                        | #
-#            sed 's/connect="/\n&/g'             | #
-#            grep '^connect="' | cut -d '"' -f 2 | #
-#            cut -c 1-2 | tr [:lower:] [:upper:] | #
-#            egrep '[A-Z0]' | tail -n 1`
-#     RIGHT=`echo $LAYERS                        | #
-#            sed 's/connect="/\n&/g'             | #
-#            grep '^connect="' | cut -d '"' -f 2 | #
-#            cut -c 3-4 | tr [:lower:] [:upper:] | #
-#            egrep '[A-Z0]' | tail -n 1`
-#    BOTTOM=`echo $LAYERS                        | #
-#            sed 's/connect="/\n&/g'             | #
-#            grep '^connect="' | cut -d '"' -f 2 | #
-#            cut -c 5-6 | tr [:lower:] [:upper:] | #
-#            egrep '[A-Z0]' | tail -n 1`
-#      LEFT=`echo $LAYERS                        | #
-#            sed 's/connect="/\n&/g'             | #
-#            grep '^connect="' | cut -d '"' -f 2 | #
-#            cut -c 7-8 | tr [:lower:] [:upper:] | #
-#            egrep '[A-Z0]' | tail -n 1`
-#       IOS="${TOP}_${RIGHT}_${BOTTOM}_${LEFT}_"
-#       DIF=`echo ${KOMBI}${IOS}.svg   | #
-#            md5sum | cut -c 1-9       | #
-#            tr -t [:lower:] [:upper:] | #
-#            rev`                        #
-#   OUTNAME=$NID$FID`echo $R$M$DIF | rev    | #
-#                    sed 's/-M[-]*R+/-MR+/' | #
-#                    rev | cut -c 1-9 | rev`_${IOS}
-# fi
-# ----------------------------------------------------------------------- #
-  SRCID=`basename $SRC       | # DISPLAY BASENAME
-         md5sum | cut -c 1-10` # GENERATE/CUT HASH
-# ----
-  HTMP=""
-  for LAYERNAME in `echo $LAYERGREP  | #
-                    sed 's/|/\n/g'`    #
-   do THASH=`echo $LAYERNAME      | # $LAYERNAME
-             sed 's/[-_][0-9]*$//'| # RM NUMBER
-             md5sum | cut -c 1-4`   # MAKE HASH
-       TNUM=`echo $LAYERNAME                        | # $LAYERNAME
-             sed 's/\(.*\)\([-_]\)\([0-9]*$\)/\3/'  | # KEEP NUMBER
-             md5sum | sed 's/[^0-9]//g' | cut -c 1-3` # NUM ONLY HASH
-      LHASH="$THASH$TNUM"
-       HTMP="${HTMP}|$LHASH"
-  done
-# ----
-  HASH=""
-  for H in `echo $HTMP     | #
-            sed 's/|/\n/g' | #
-            sort -u`
-  do HASH="${HASH}|$H";done
-     HASH=`echo $HASH | sed 's/^|//g'`
+   do 
+      KOMBI=`echo $KOMBI | sed 's/::SP::/ /g'`
+      LAYERGREP=`echo $KOMBI | sed 's/ /|/g'`
+      LAYERS=`echo ${SRCSRC}            | #
+              sed 's/<g/\n&/g'          | # GROUPS ON NEWLINE
+              sed '/^<g/s/>/&\n/g'      | # FIRST '>' ON NEWLINE
+              grep ':groupmode="layer"' | #
+              egrep "$LAYERGREP"`         #   
+      # ---
+        TOP=`echo $LAYERS                        | #
+             sed 's/connect="/\n&/g'             | #
+             grep '^connect="' | cut -d '"' -f 2 | #
+             cut -c 1-2 | tr [:lower:] [:upper:] | #
+             egrep '[A-Z0]' | tail -n 1`
+      RIGHT=`echo $LAYERS                        | #
+             sed 's/connect="/\n&/g'             | #
+             grep '^connect="' | cut -d '"' -f 2 | #
+             cut -c 3-4 | tr [:lower:] [:upper:] | #
+             egrep '[A-Z0]' | tail -n 1`
+     BOTTOM=`echo $LAYERS                        | #
+             sed 's/connect="/\n&/g'             | #
+             grep '^connect="' | cut -d '"' -f 2 | #
+             cut -c 5-6 | tr [:lower:] [:upper:] | #
+             egrep '[A-Z0]' | tail -n 1`
+       LEFT=`echo $LAYERS                        | #
+             sed 's/connect="/\n&/g'             | #
+             grep '^connect="' | cut -d '"' -f 2 | #
+             cut -c 7-8 | tr [:lower:] [:upper:] | #
+             egrep '[A-Z0]' | tail -n 1`
+        IOS="${TOP}_${RIGHT}_${BOTTOM}_${LEFT}"
+      # ---
+        R000="${C1}_${C2}_${C3}_${C4}"
+        R090="${C2}_${C3}_${C4}_${C1}"
+        R180="${C3}_${C4}_${C1}_${C2}"
+        R270="${C4}_${C1}_${C2}_${C3}"
 
-  CHECKHASH=`echo $SRCID   | #
-             cut -c 1-3`     #
-  for H in `echo $HASH     | #
-            sed 's/|/\n/g' | #
-            sort -u`         #
-   do CHECKHASH="${CHECKHASH}"`echo $H | #
-                 sed 's/\(.\)\(.\)\(.\)\(.\)\(.\)\(.\)\(.\)/\1\5\7/g'`
-  done
-# ----
-  SEED=`echo $LAYERGREP | sed 's/[^|]*/000/g' | sed 's/|//g'`
-  TIME=`expr $TIME + 1`
-# ----------------------------------------------------------------------- #
-  echo "$CHECKHASH|$SRCID|$HASH|$SEED|$TIME"
-# ----------------------------------------------------------------------- #
+        if [ `basename $SRC | grep "$MAYROTATE" | wc -l` -gt 0 ]
+        then  CGREP="$R000|$R090|$R180|$R270"
+        elif [ `basename $SRC | grep "$MAYFLIP" | wc -l` -gt 0 ]
+        then  CGREP="$R000|$R180"
+        else  CGREP="$R000"
+        fi
+
+ # ======================================================================= #
+   if [ `echo $IOS | egrep "$CGREP" | wc -l` -gt 0 ]
+   then
+ # ----------------------------------------------------------------------- #
+   SRCID=`basename $SRC       | # DISPLAY BASENAME
+          md5sum | cut -c 1-10` # GENERATE/CUT HASH
+ # ----
+   HTMP=""
+   for LAYERNAME in `echo $LAYERGREP  | #
+                     sed 's/|/\n/g'`    #
+    do THASH=`echo $LAYERNAME      | # $LAYERNAME
+              sed 's/[-_][0-9]*$//'| # RM NUMBER
+              md5sum | cut -c 1-4`   # MAKE HASH
+        TNUM=`echo $LAYERNAME                        | # $LAYERNAME
+              sed 's/\(.*\)\([-_]\)\([0-9]*$\)/\3/'  | # KEEP NUMBER
+              md5sum | sed 's/[^0-9]//g' | cut -c 1-3` # NUM ONLY HASH
+       LHASH="$THASH$TNUM"
+        HTMP="${HTMP}|$LHASH"
+   done
+ # ----
+   HASH=""
+   for H in `echo $HTMP     | #
+             sed 's/|/\n/g' | #
+             sort -u`
+   do HASH="${HASH}|$H";done
+      HASH=`echo $HASH | sed 's/^|//g'`
+ 
+   CHECKHASH=`echo $SRCID   | #
+              cut -c 1-3`     #
+   for H in `echo $HASH     | #
+             sed 's/|/\n/g' | #
+             sort -u`         #
+    do CHECKHASH="${CHECKHASH}"`echo $H | #
+                  sed 's/\(.\)\(.\)\(.\)\(.\)\(.\)\(.\)\(.\)/\1\5\7/g'`
+   done
+ # ----
+   SEED=`echo $LAYERGREP | sed 's/[^|]*/000/g' | sed 's/|//g'`
+   TIME=`expr $TIME + 1`
+ # ----------------------------------------------------------------------- #
+   echo "$CHECKHASH|$SRCID|$HASH|$SEED|$TIME"
+ # ----------------------------------------------------------------------- #
+   fi
+ # ======================================================================= #
+
  done
 
 exit 0;
