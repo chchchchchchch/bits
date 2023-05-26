ff46d8b0eeb20c4f36d05b769d09945a6920b2e6
christoph <christoph@lafkon.net>
2023-05-26 12:10:47 +0200

    stop as explained here https://www.youtube.com/watch?v=NV6YHZ2RAqc

--- 2023/Arduino/MG995/MG995.ino
+++ 2023/Arduino/MG995/MG995.ino
@@ -1,11 +1,11 @@
 /* This example Arduino Sketch controls the complete rotation of
- *  SG995 Servo motor by using its PWM and Pulse width modulation technique
+ * MG995 Servo motor by using its PWM and Pulse width modulation technique
  */
  
 #include <Servo.h>  // include servo library to use its related functions
 #define Servo_PWM 6 // A descriptive name for D6 pin of Arduino to provide PWM signal
 Servo MG995_Servo;  // Define an instance of of Servo with the name of "MG995_Servo"
-  
+ 
 void setup() {
   Serial.begin(9600);            // Initialize UART with 9600 Baud rate
   MG995_Servo.attach(Servo_PWM); // Connect D6 of Arduino with PWM signal pin of servo motor
@@ -13,17 +13,15 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("RIGHT");       // You can display on the serial the signal value
-  MG995_Servo.write(0);          // Turn clockwise at high speed
+  Serial.println("CW");  // You can display on the serial the signal value
+  MG995_Servo.write(0);  // Turn clockwise
   delay(3000);
-  MG995_Servo.detach();          // Stop. You can use deatch function or use write(x), as x is the middle of 0-180 which is 90, but some lack of precision may change this value
+  MG995_Servo.write(90); // Stop.
   delay(2000);
-  MG995_Servo.attach(Servo_PWM); // Always use attach function after detach to re-connect your servo with the board
-  Serial.println("LEFT");        // Turn left high speed
+  Serial.println("CCW"); // Turn counterclockwise
   MG995_Servo.write(180);
   delay(3000);
-  MG995_Servo.detach();          // Stop
+  MG995_Servo.write(90); // Stop
   delay(2000);
-  MG995_Servo.attach(Servo_PWM);
-      
+
 }
