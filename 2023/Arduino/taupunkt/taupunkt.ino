eeee98ed61c758761b615a22c86478a9b1e57573
christoph <christoph@lafkon.net>
2023-10-26 12:02:50 +0200

    keep testing

--- 2023/Arduino/taupunkt/taupunkt.ino
+++ 2023/Arduino/taupunkt/taupunkt.ino
@@ -2,23 +2,36 @@
 
 #define DHTPIN_0 2
 #define DHTPIN_I 4
-
 #define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
 
 DHT dht_O(DHTPIN_0, DHTTYPE);
 DHT dht_I(DHTPIN_I, DHTTYPE);
 
+const int RELAISPIN   =  7;
+const int MOSFETPIN_1 =  9;
+const int MOSFETPIN_2 = 10;
+
+const int POTIPIN = A0; // Einlesen des Spannungswertes am Potentiometer
+float valPoti = 0; // Poti-Wert initial auf 0 setzen
+
+
 void setup() {
-  
+
+  TCCR1B = TCCR1B & 0b11111000 | 0x01;
+
   Serial.begin(9600);
 
   dht_O.begin();
   dht_I.begin();
-  
+
+  pinMode(RELAISPIN, OUTPUT);
+  pinMode(MOSFETPIN_1,OUTPUT);
+
 }
 
 void loop() {
 
+/*
   // Wait a few seconds between measurements.
   delay(2000);
 
@@ -30,13 +43,11 @@ void loop() {
   float t_O = dht_O.readTemperature();
   float t_I = dht_I.readTemperature();
 
-/*
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h_O) || isnan(t_O)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-*/
+//// Check if any reads failed and exit early (to try again).
+//if (isnan(h_O) || isnan(t_O)) {
+//  Serial.println(F("Failed to read from DHT sensor!"));
+//  return;
+//}
 
   Serial.print(F("Humidity outside: "));
   Serial.print(h_O);
@@ -51,5 +62,16 @@ void loop() {
   Serial.print(t_I);
   Serial.print(F("°C "));
   Serial.println("");
+*/
+
+/*
+  fan_I(1);
+  delay(4000);
+  fan_I(0);
+*/
+
+  valPoti = analogRead(POTIPIN)/1023.0; // analogRead liest Werte beim Uno zwischen 0 und 1023 ein, analog Write gibt jedoch nur Werte zwischen 0 und 255 aus
+
+  fan_O(valPoti);
 
 }
---
+++ 2023/Arduino/taupunkt/utils.ino
@@ -0,0 +1,23 @@
+void fan_I( float speed ) {
+
+  if (speed > 0) {
+
+    digitalWrite(RELAISPIN, HIGH);
+    
+  } else {
+
+    digitalWrite(RELAISPIN, LOW);    
+  }
+
+}
+
+const float fan_O_speedMin = 0.05; // REQUIRES 0.1 TO START
+const float fan_O_speedMax = 1.00;
+
+void fan_O( float speed ) {
+
+  Serial.println(speed); 
+
+  speed = 255 * speed;
+  analogWrite(MOSFETPIN_1, speed);
+}
