2de7af53494953e1aa687d4ad5387db438204033
christoph <christoph@lafkon.net>
2023-10-31 18:36:14 +0100

    keep testing + add calculation from https://www.feuerwerk-forum.de/thema/taupunktlueftung-trockenlueftung-mit-arduino.54105

--- 2023/Arduino/taupunkt/taupunkt.ino
+++ 2023/Arduino/taupunkt/taupunkt.ino
@@ -14,6 +14,14 @@ const int MOSFETPIN_2 = 10;
 const int POTIPIN = A0; // Einlesen des Spannungswertes am Potentiometer
 float valPoti = 0; // Poti-Wert initial auf 0 setzen
 
+float h_MAX = 70.0; // max. Luftfeuchte Innen
+float dT = 5.0;     // Temp. Delta für Taupunktberechnung
+
+float dp;
+float h_I;
+float h_O;
+float t_I;
+float t_O;
 
 void setup() {
 
@@ -31,7 +39,17 @@ void setup() {
 
 void loop() {
 
+  valPoti = analogRead(POTIPIN)/1023.0; // analogRead liest Werte beim Uno zwischen 0 und 1023 ein, analog Write gibt jedoch nur Werte zwischen 0 und 255 aus
+  fan_O(valPoti);
+
 /*
+  if ( valPoti > 0.9 ) {
+    fan_I(1);
+  } else {
+    fan_I(0);    
+  }
+*/
+
   // Wait a few seconds between measurements.
   delay(2000);
 
@@ -49,6 +67,35 @@ void loop() {
 //  return;
 //}
 
+  float a, b;
+  if (t_O >= 0) {
+    a = 7.5;
+    b = 237.3;
+  } else {
+    a = 7.6;
+    b = 240.7;
+  }
+
+  float sgp = 6.1078 * pow(10, (a * t_O) / (b + t_O));
+  float gp = (t_O / 100) * sgp;
+  float v = log10(gp / 6.1078);
+
+  dp = (b * v) / (a - v);
+
+  if ((dp - dT) < t_I) {
+    //FAN_I = 1;
+    //fan_I(1);
+    Serial.println("fan_I ON");
+  } else {
+    //FAN_I = 0;
+    //fan_I(0);
+    Serial.println("fan_I OFF");
+  }
+  if (h_O > h_MAX)  fan_I(0); // Feuchte aussen zu hoch
+
+
+
+
   Serial.print(F("Humidity outside: "));
   Serial.print(h_O);
   Serial.print(F("%  temperature outside: "));
@@ -62,16 +109,5 @@ void loop() {
   Serial.print(t_I);
   Serial.print(F("°C "));
   Serial.println("");
-*/
-
-/*
-  fan_I(1);
-  delay(4000);
-  fan_I(0);
-*/
-
-  valPoti = analogRead(POTIPIN)/1023.0; // analogRead liest Werte beim Uno zwischen 0 und 1023 ein, analog Write gibt jedoch nur Werte zwischen 0 und 255 aus
-
-  fan_O(valPoti);
 
 }
--- 2023/Arduino/taupunkt/utils.ino
+++ 2023/Arduino/taupunkt/utils.ino
@@ -3,7 +3,8 @@ void fan_I( float speed ) {
   if (speed > 0) {
 
     digitalWrite(RELAISPIN, HIGH);
-    
+    delay(100);
+   
   } else {
 
     digitalWrite(RELAISPIN, LOW);    
@@ -16,8 +17,8 @@ const float fan_O_speedMax = 1.00;
 
 void fan_O( float speed ) {
 
-  Serial.println(speed); 
-
+  //Serial.println(speed); 
   speed = 255 * speed;
   analogWrite(MOSFETPIN_1, speed);
+
 }
